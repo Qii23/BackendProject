aa5e51cd24fa75f8991f81c25578d427aff613ba
Qii23 <rafqiizi23@gmail.com>
2020-10-29 16:11:50 +0700

    feat<login>: add login controller

--- all/controller/login.py
+++ all/controller/login.py
@@ -5,14 +5,14 @@ import json
 import uuid
 import pymysql.cursors
 
-def login(name):
-	mydict = create_dict()
-	mycursor.execute("""SELECT * FROM User WHERE name = %s""",(name))
-	rv = mycursor.fetchall()
-	payload = []
-	content = {}
-	for result in rv:
-		content = {'id': result[1], 'name': result[2], 'gender': result[3], 'role': result[4]}
-		payload.append(content)
-		content = {}
-	return jsonify(payload)
+def logins(email,password):
+	mycursor.execute("""SELECT * FROM User WHERE email = %s and password =%s""",(email,password))
+	if(mycursor.rowcount>0):
+		mycursor.execute("""SELECT * FROM User WHERE email = %s""",(email))
+		rv = mycursor.fetchall()
+		payload = []
+		for result in rv:
+			payload.append(result)
+		return json.dumps(payload)
+	else:
+		return("wrong email or password")
