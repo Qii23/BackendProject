1382dea2e53e6f32d19918ad9474c56e7aa25bdd
Qii23 <rafqiizi23@gmail.com>
2020-10-29 16:44:03 +0700

    chore<main>: add routing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from all.database.connection import mycursor,mydb
 from flask import Flask, jsonify
-from all.controller.register import register
+from all.controller.register import registers
+from all.controller.login import logins
+from all.controller.products import inputproduct,updateproductprice,updateproductquantity,deleteproduct
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 import json
 import uuid
@@ -8,17 +10,30 @@ import pymysql.cursors
 
 app = Flask(__name__)
 
-@app.route("/register/<name>/<gender>", methods=['POST'])
-def register(name,gender):
-	return register(name,gender)
+@app.route("/register/<name>/<gender>/<email>/<password>", methods=['POST'])
+def register(name,gender,email,password):
+	return registers(name,gender,email,password)
 
-@app.route("/login/<name>", methods=['GET'])
-def login(name):
-	return login(name)
+@app.route("/login/<email>/<password>", methods=['GET'])
+def login(email,password):
+	return logins(email,password)
+
+@app.route("/insertpr/<name>/<quantity>/<price>", methods=['POST'])
+def insertpr(name,quantity,price):
+	return inputproduct(name,quantity,price)
+
+@app.route("/updateproductquantity/<name>/<quantity>", methods=['PUT'])
+def updateprqty(name,quantity):
+	return updateproductquantity(name,quantity)
+
+@app.route("/updateproductprice/<name>/<price>", methods=['PUT'])
+def updateprprc(name,price):
+	return updateproductprice(name,quantity)
+
+@app.route("/deleteproduct/<name>", methods=['DELETE'])
+def deletepr(name):
+	return deleteproduct(name)
 	
-mycursor.execute("SELECT * FROM User")
-x = mycursor.fetchall()
-
 for i in x:
 	print(i)
 
