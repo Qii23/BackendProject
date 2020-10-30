ae24f3ccd3649ce3fca3e6fed55cfe20179c1e90
Qii23 <rafqiizi23@gmail.com>
2020-10-30 21:34:43 +0700

    feat(register): add register controller

--- all/controller/register.py
+++ all/controller/register.py
@@ -2,12 +2,16 @@ from all.database.connection import mycursor,mydb
 import json
 import pymysql.cursors
 
-def registers(name,gender,email,password):
-	mycursor.execute("""SELECT * FROM User WHERE email = %s""",(email))
+def ambilemail(email):
+		mycursor.execute("""SELECT * FROM User WHERE email = %s""",(email))
+		return mycursor
+
+def register(name,gender,email,password):
+	ambilemail(email)
 	if(mycursor.rowcount==0):
 		mycursor.execute("INSERT INTO User(name,gender,email,password,role) VALUES (%s,%s,%s,%s,%s)",(name,gender,email,password,"Client"))
 		mydb.commit()
-		mycursor.execute("""SELECT * FROM User WHERE email = %s""",(email))
+		ambilemail(email)
 		rv = mycursor.fetchall()
 		payload = []
 		for result in rv:
@@ -15,3 +19,4 @@ def registers(name,gender,email,password):
 		return json.dumps(payload)
 	else:
 		return("email already registered")
+		
